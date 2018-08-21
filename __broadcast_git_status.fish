c8b6d5187f174232650d7d61d53aa61e9ceac274
Tom Conchie <tconchie@greenchef.com>
2018-08-21 17:01:31 -0600

    [update] Broadcast git status on dir change now shows an emoji for whether the status is clean. Same with prompt

--- __broadcast_git_status.fish
+++ __broadcast_git_status.fish
@@ -1,5 +1,12 @@
 function __broadcast_git_status -v PWD
-    if test -d .git
-        echo Currently on branch (set_color red)'☱'(set_color yellow)'☲'(set_color blue)'☴' (set_color green) (git rev-parse --abbrev-ref HEAD) (set_color blue)'☴'(set_color yellow)'☲'(set_color red)'☱' 
+    set changes (git status -s) > /dev/null ^ /dev/null
+    if test -z $changes
+        set changeEmoji "✅  "
+    else
+        set changeEmoji "⚠️  "
+    end
+    git status > /dev/null ^ /dev/null
+    if test $status -eq 0
+        echo Currently on branch (set_color cyan)'☱'(set_color yellow)'☲'(set_color magenta)'☴' (set_color green) (git rev-parse --abbrev-ref HEAD) $changeEmoji  (set_color magenta)'☴'(set_color yellow)'☲'(set_color cyan)'☱' 
     end
 end
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,7 +3,12 @@ function fish_prompt
     and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
     test "$USER" = 'root'
     and echo (set_color red)"#"
-
+    set changes (git status -s) > /dev/null ^ /dev/null
+    if test -z $changes
+        set changeEmoji "✅  "
+    else
+        set changeEmoji "⚠️  "
+    end
     # Main
-    echo -n (set_color cyan)(prompt_pwd)🦑 (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo -n (set_color cyan)(prompt_pwd)' 🦑  '$changeEmoji(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
