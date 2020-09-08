3e678b04fc0bfe30ecfae348c968d5aa85274edc
tom-con <tconchie@greenchef.com>
2020-09-08 10:20:01 -0600

    adds in nvm and tweaks gdh

--- gdh.fish
+++ gdh.fish
@@ -1,12 +1,12 @@
 function gdh
     if [ -n "$argv[1]" ]
         if [ -e "$argv[1]" ] and not [ -e "$argv[2]" ]
-            git diff head $argv[1]
+            git diff HEAD~$argv[1]
         else
-            git diff $argv[2] $argv[1]
+            git diff $argv[2]~$argv[1]
         end
     else
-        git diff head
+        git diff HEAD
     end
 end
 
